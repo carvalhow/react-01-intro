cbd024bed89dd533738da463a5df8784ecf360f1
Wesley Carvalho <wesley.carvalho@jnmoura.com.br>
2020-01-28 16:28:31 -0300

    Creating repository

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import profile from './assets/0001.jpg';
+import TechList from './components/TechList';
+
+function App() {
+  return <TechList />;
+}
+
+export default App;
